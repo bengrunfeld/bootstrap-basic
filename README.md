bbf30646b1a77ba0c6f3541791a70f2f4f8c5134
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-10-29 09:18:54 -0600

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bootstrap Basic Files 
+
+These are the basic files needed to create a bare-bones Bootstrap app. Bootstrap css and js are not included in this repo on purpose, forcing the user to download bootstrap themselves.
+
+
+
+
---
+++ js/columns.js
@@ -0,0 +1,14 @@
+// Store button functionality
+document.getElementsByClassName("store-btn")[0].addEventListener("click", function(){
+    console.log('The store button was clicked');
+});
+
+// Product button functionality
+document.getElementsByClassName("product-btn")[0].addEventListener("click", function(){
+    console.log('The product button was clicked');
+});
+
+// Features button functionality
+document.getElementsByClassName("features-btn")[0].addEventListener("click", function(){
+    console.log('The features button was clicked');
+});
